4bb62353704600ffdd62b6cccb8ac323cb95a27c
Michael Law <michael@mlwdltd.onmicrosoft.com>
2020-04-11 17:36:28 +0100

    Lots of round now added too

--- CardsAgainstHumanity.Api/FunctionSignalr.cs
+++ CardsAgainstHumanity.Api/FunctionSignalr.cs
@@ -4,6 +4,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using Newtonsoft.Json;
 
 namespace CardsAgainstHumanity.Api
 {
@@ -22,12 +23,13 @@ namespace CardsAgainstHumanity.Api
             [QueueTrigger("game-amended")] Game game,
             [SignalR(HubName = "cah")] IAsyncCollector<SignalRMessage> signalRMessages)
         {
+            var gameData = JsonConvert.SerializeObject(game);
+
             await signalRMessages.AddAsync(
                 new SignalRMessage
                 {
                     Target = "gameUpdated",
-                    GroupName = game.Url,
-                    Arguments = new [] { game }
+                    Arguments = new [] { gameData }
                 });
         }
     }
