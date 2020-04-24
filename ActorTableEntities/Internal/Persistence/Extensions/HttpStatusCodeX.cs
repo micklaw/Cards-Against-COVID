80ef73f42789c9dc5050ca82247fbced2e9c0fea
Michael Law <michael@mlwdltd.onmicrosoft.com>
2020-04-24 16:58:25 +0100

    Publishing with nuget

--- ActorTableEntities/Internal/Persistence/Extensions/HttpStatusCodeX.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ActorTableEntities.Internal.Persistence.Extensions
-{
-    internal static class HttpStatusCodeX
-    {
-        public static bool IsSuccess(this int code) => (code >= 200 && code <= 299);
-    }
-}
